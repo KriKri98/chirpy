614c359bb53ff45ae2550f59a150276ed79ee594
Michael <1000lpkrikri@gmail.com>
2026-07-30 15:43:56 +0200

    added readinessEndpoint

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadinessEndpoint)
 	s := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 	s.ListenAndServe()
 
+}
+
+func handlerReadinessEndpoint(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(200)
+	res.Write([]byte("OK"))
 }
